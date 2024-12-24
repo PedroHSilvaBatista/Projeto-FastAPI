ce306206b3d5e8554a8f5dca0aebbf187ad82757
Pedro <pedrohenriquesb02@gmail.com>
2024-12-24 00:24:19 -0300

    Modelando a classe Biblioteca e simulando um banco de dados com json

--- models/biblioteca.py
+++ models/biblioteca.py
@@ -1,17 +1,39 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Union
 
 
 class Biblioteca(BaseModel):
-    nome: str
-    ano_de_publicacao: int
-    autor: str
-    descricao: Optional[str] = None
-    generos: List[str]
+    _nome: str
+    _ano_de_publicacao: int
+    _autor: str
+    _descricao: Union[str, None] = None
+    _generos: List[str]
     
-
     def __str__(self):
-        return f'{self.nome}, {self.descricao}, {self.generos}'
+        return f'{self._nome}, {self._descricao}, {self._generos}'
+    
+    @property
+    def get_nome(self):
+        return self._nome
+
+    @property
+    def get_ano_de_publicacao(self):
+        return self._ano_de_publicacao
+    
+    @property
+    def get_autor(self):
+        return self._autor
+
+    @property
+    def get_descricao(self):
+        if self._descricao is not None:
+            return self._descricao
+        return None
+
+    @property
+    def get_generos(self):
+        return self._generos
+
     
 
 livro = Biblioteca(
